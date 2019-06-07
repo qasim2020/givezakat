6441bde3eb299247002a74fec9f71c1247b779c6
Qasim <qasimali24@gmail.com>
2019-06-08 03:24:42 +0500

    pasted sign in / up / forgot pw func from old project.

--- index.js
+++ index.js
@@ -95,6 +95,92 @@ app.post('/excelData',(req,res) => {
 
 });
 
+app.post('/data',(req,res) => {
+
+  if (req.body.query === 'Register') {
+    var user = new Users(
+      _.pick(req.body,['name','email','password']),
+    );
+    user.generateAuthToken().then((returned) => {
+      res.status(200).send(returned.tokens[0].token);
+      return console.log('saved', returned.name);
+    }).catch((e) => {
+      console.log(e);
+      if (e.code === 11000) return res.status(400).send('You are already registered with this email');
+      console.log('Error here', e);
+      return res.status(400).send('Server - Bad Request');
+    });
+  };
+
+  if (req.body.query === 'Login') {
+    var user = _.pick(req.body,['email','password']);
+    Users.findByCredentials(user.email, user.password).then((returned) => {
+      if (!returned) return Promise.reject('Invalid credentials.')
+      return returned.generateAuthToken();
+    }).then((user) => {
+      if (!user) return Promise.reject('Failed to Sign In.');
+      return res.status(200).send(user.tokens[0].token);
+    }).catch((e) => {
+      console.log(e);
+      res.status(404).send(e);
+    });
+  };
+
+  if (req.body.query === 'Email_Verify') {
+    var phoneCode = Math.floor(100000 + Math.random() * 900000);
+    Users.findOne({"email":req.body.email}).then((user) => {
+      if (!user) return res.status(404).send('Un authorized request');
+      return sendmail(req.body.email,`Your Code is <b>${phoneCode}</b>, please enter it on webpage.`,'Make a story - Forgot Password')
+    }).then((msg) => {
+      return Users.findOneAndUpdate({"email": req.body.email}, {$set : {"phoneCode":phoneCode}}, {new: true});
+    }).then((user) => {
+      res.status(200).send('Mail sent !');
+    }).catch((e) => {
+      console.log(e);
+      res.status(404).send(e.errno);
+    });
+  };
+
+  if (req.body.query === 'Test_Code') {
+    Users.findOne({
+      "email": req.body.email,
+      "phoneCode": req.body.code
+    }).then((user) => {
+      if (!user) return Users.findOneAndUpdate({"email":req.body.email},{$set: {attemptedTime: new Date()}, $inc:{wrongAttempts:1}},{new:true});
+      console.log('user found', user);
+      return Promise.resolve('Found');
+    }).then((response) => {
+      if (response === 'Found') return res.status(200).send('Verified !');
+      console.log(`${response}:::: Wrong attempt no: ${response.wrongAttempts} x Attempt`);
+      if (response.wrongAttempts > 4) return res.status(404).send('5 wrong attempts, please try again after 2 mins.');
+      return res.status(400).send('Did not match !');
+    }).catch((e) => {
+      console.log(e);
+      return res.status(400).send(e);
+    });
+  };
+
+  if (req.body.query === 'new_password') {
+    Users.findOne({
+      "email": req.body.email,
+      "phoneCode": req.body.code,
+    }).then((user) => {
+      if (!user) return Promise.reject('Un authorized request.');
+      user.password = req.body.password;
+      return user.generateAuthToken();
+    }).then((response)=> {
+      console.log(response);
+      res.status(200).send('Password changed, please login with new password.');
+    }).catch((e) => {
+      console.log(e);
+      res.status(404).send(e);
+    });
+
+  };
+
+});
+
+
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
---
+++ models/users.js
@@ -0,0 +1,118 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    }
+  }],
+  phoneCode: {
+    type: String,
+  },
+  wrongAttempts: {
+    type: Number,
+    default: 0,
+  },
+  attemptedTime: {
+    type: Number,
+    default: 0,
+  }
+});
+
+UsersSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+UsersSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.updateOne({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
+UsersSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
+
+  user.tokens = {access, token};
+
+  return user.save().then(() => {
+    return user;
+  });
+};
+
+UsersSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
+UsersSchema.statics.findByCredentials = function (email, password) {
+
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) return Promise.reject('No user found with this email and password. Please sign up.');
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject('Password did not match !');
+          }
+        });
+      });
+    });
+
+  };
+
+
+var Users = mongoose.model('Users', UsersSchema);
+
+module.exports = {Users};
