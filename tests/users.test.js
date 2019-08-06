0880681d8f5d0800aa527c2cf4a0e118f99ca9c2
Qasim Ali <qasimali24@gmail.com>
2019-08-06 19:00:24 +0500

    create basic home page test to understand jest

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,10 +2,23 @@ require('../config/config');
 
 const express = require('express');
 const request = require('supertest');
-const {app} = require('../index.js');
+const {app} = require('../app.js');
 
-test('Should open basic home page', (done) => {
+test('Should open home page with basic data', (done) => {
   request(app)
     .get('/')
-    .expect(200)
+    .set('Accept', 'test_call')
+    .expect((res) => {
+      expect(res.body.sampleRows.length).toBe(9);
+      expect(res.body.due).toBe(0);
+      expect(res.body).toHaveProperty('dueIds');
+      expect(res.body.currency).toBeFalsy();
+      expect(res.body).toHaveProperty('count');
+    })
+    .expect(200, done)
 });
+
+test('Should create a new user by google', (done) => {
+  request(app)
+    .get('/')
+})
